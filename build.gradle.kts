f3f8d67a27c5b1fe800e85e9734da21f6b0c51db
Guilherme Sampaio <guisamfurquim@gmail.com>
2024-10-07 16:34:48 -0300

    AutenticaÃção, ainda não achei onde gera token

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,13 +20,17 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.security:spring-security-config:6.3.3")
+	implementation("org.springframework.security:spring-security-web:6.3.3")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ src/main/kotlin/br/pucpr/authserver/security/SecurityConfig.kt
@@ -0,0 +1,54 @@
+package br.pucpr.authserver.security
+
+import jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(security: HttpSecurity): DefaultSecurityFilterChain =
+
+        security
+            .cors { it.disable() }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .exceptionHandling { it
+                .authenticationEntryPoint { _, response, authException ->
+                    response.sendError(SC_UNAUTHORIZED, authException.message)
+                }
+            }
+            .headers { it
+                .frameOptions { frameOptions -> frameOptions.disable() }
+            }
+            .authorizeHttpRequests { requests ->
+                requests
+                    .requestMatchers(HttpMethod.GET).permitAll()
+                    .requestMatchers("/error/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/users", "/users/login").permitAll()
+                    .requestMatchers("/h2-console/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .build()
+
+    @Bean
+    fun corsFilter() =
+        CorsConfiguration().apply{
+            addAllowedHeader("*")
+            addAllowedOrigin("*")
+            addAllowedMethod("*")
+        } .let{
+            UrlBasedCorsConfigurationSource().apply {
+                registerCorsConfiguration("/**", it)
+            }
+        } .let { CorsFilter(it) }
+}
